7506e4093fbb5c4a38f96088cafcc9152610ce0a
Gustavo Coronado <gustavo_15334@hotmail.com>
2022-11-28 23:43:39 -0600

    tienda y pag producto

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function ValidateEmail() {
     }
 
 }
+
+function ValidateEmailForm() {
+    var mail = document.getElementById("email").value;
+    var validRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(validRegex.test(mail)) {
+        alert("¡Su mensaje ha sido enviado con éxito!");
+        return true;
+    }
+    else {
+        alert("Ingrese un e-mail válido e inténtelo de nuevo");
+        return false;
+    }
+
+}
